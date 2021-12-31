7c29b84c04578a7b4cfb0703f3a863af4b715210
jeremie <permentierj@gmail.com>
2021-12-31 15:17:58 +0100

    premier commit pour mise en ligne

---
+++ piano.js
@@ -0,0 +1,37 @@
+const touchs = document.querySelectorAll('.piano__key');
+
+const play = (e) => {
+    console.log(e)
+    let key = null;
+    if (e.pointerType === "mouse" || e.pointerType === "touch") {
+        key = e.target.attributes[0].value;
+    } else {
+        key = e.keyCode;
+    }
+    let sound = document.querySelector(`audio[data-key="${key}"]`);
+    let touch = document.querySelector(`button[data-key="${key}"]`);
+
+    if (!sound) return;
+    sound.currentTime = 0;
+    sound.play();
+
+
+    if (touch.classList[1] === 'white') {
+        touch.classList.add('play__white');
+    } else {
+        touch.classList.add('play__black');
+    }
+}
+
+const removeTransition = (e) => {
+    if (e.propertyName !== "transform") return;
+    if(e.target.classList.contains('play__white')) {
+        e.target.classList.remove('play__white');
+    } else {
+        e.target.classList.remove('play__black');
+    }
+}
+
+touchs.forEach(touch => touch.addEventListener('transitionend', removeTransition));
+touchs.forEach(touch => touch.addEventListener('click', play));
+window.addEventListener('keydown', play);
