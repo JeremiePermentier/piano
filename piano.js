b188d331fd0be6053b2cb13e9a1c39e907073743
jeremie <permentierj@gmail.com>
2021-12-31 15:48:13 +0100

    Modification des fonctions pour rendre compatible avec safari

--- piano.js
+++ piano.js
@@ -1,6 +1,7 @@
 const touchs = document.querySelectorAll('.piano__key');
 
-const play = (e) => {
+
+function play (e) {
     console.log(e)
     let key = null;
     if (e.pointerType === "mouse" || e.pointerType === "touch") {
@@ -23,7 +24,7 @@ const play = (e) => {
     }
 }
 
-const removeTransition = (e) => {
+function removeTransition  (e) {
     if (e.propertyName !== "transform") return;
     if(e.target.classList.contains('play__white')) {
         e.target.classList.remove('play__white');
@@ -31,6 +32,38 @@ const removeTransition = (e) => {
         e.target.classList.remove('play__black');
     }
 }
+
+// const play = (e) => {
+//     console.log(e)
+//     let key = null;
+//     if (e.pointerType === "mouse" || e.pointerType === "touch") {
+//         key = e.target.attributes[0].value;
+//     } else {
+//         key = e.keyCode;
+//     }
+//     let sound = document.querySelector(`audio[data-key="${key}"]`);
+//     let touch = document.querySelector(`button[data-key="${key}"]`);
+
+//     if (!sound) return;
+//     sound.currentTime = 0;
+//     sound.play();
+
+
+//     if (touch.classList[1] === 'white') {
+//         touch.classList.add('play__white');
+//     } else {
+//         touch.classList.add('play__black');
+//     }
+// }
+
+// const removeTransition = (e) => {
+//     if (e.propertyName !== "transform") return;
+//     if(e.target.classList.contains('play__white')) {
+//         e.target.classList.remove('play__white');
+//     } else {
+//         e.target.classList.remove('play__black');
+//     }
+// }
 
 touchs.forEach(touch => touch.addEventListener('transitionend', removeTransition));
 touchs.forEach(touch => touch.addEventListener('click', play));
