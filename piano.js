75a5b4a42754d2dbe8d78d8fc9b72e44e1466a87
jeremie <permentierj@gmail.com>
2021-12-31 15:56:09 +0100

    test de pour ios

--- piano.js
+++ piano.js
@@ -1,7 +1,6 @@
 const touchs = document.querySelectorAll('.piano__key');
 
-
-function play (e) {
+const play = (e) => {
     console.log(e)
     let key = null;
     if (e.pointerType === "mouse" || e.pointerType === "touch") {
@@ -24,7 +23,7 @@ function play (e) {
     }
 }
 
-function removeTransition  (e) {
+const removeTransition = (e) => {
     if (e.propertyName !== "transform") return;
     if(e.target.classList.contains('play__white')) {
         e.target.classList.remove('play__white');
@@ -33,38 +32,11 @@ function removeTransition  (e) {
     }
 }
 
-// const play = (e) => {
-//     console.log(e)
-//     let key = null;
-//     if (e.pointerType === "mouse" || e.pointerType === "touch") {
-//         key = e.target.attributes[0].value;
-//     } else {
-//         key = e.keyCode;
-//     }
-//     let sound = document.querySelector(`audio[data-key="${key}"]`);
-//     let touch = document.querySelector(`button[data-key="${key}"]`);
-
-//     if (!sound) return;
-//     sound.currentTime = 0;
-//     sound.play();
-
-
-//     if (touch.classList[1] === 'white') {
-//         touch.classList.add('play__white');
-//     } else {
-//         touch.classList.add('play__black');
-//     }
-// }
-
-// const removeTransition = (e) => {
-//     if (e.propertyName !== "transform") return;
-//     if(e.target.classList.contains('play__white')) {
-//         e.target.classList.remove('play__white');
-//     } else {
-//         e.target.classList.remove('play__black');
-//     }
-// }
+const test = () => {
+    alert('ok')
+}
 
 touchs.forEach(touch => touch.addEventListener('transitionend', removeTransition));
 touchs.forEach(touch => touch.addEventListener('click', play));
+touchs.forEach(touch => touch.addEventListener('touchstart', test));
 window.addEventListener('keydown', play);
